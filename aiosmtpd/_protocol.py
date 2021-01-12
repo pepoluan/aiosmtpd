5cbed15c17d3dcab71e796ddaf59fa30782fbd25
Pandu POLUAN <pepoluan@gmail.com>
2021-01-12 15:20:36 +0700

    Remove code/logic not used by aiosmtpd

--- aiosmtpd/_protocol.py
+++ aiosmtpd/_protocol.py
@@ -4,17 +4,11 @@ __all__ = [
     "StreamReader",
     "StreamWriter",
     "StreamReaderProtocol",
-    "open_connection",
-    "start_server",
     "IncompleteReadError",
     "LimitOverrunError",
 ]
 
-import socket
 import logging
-
-if hasattr(socket, "AF_UNIX"):
-    __all__.extend(["open_unix_connection", "start_unix_server"])
 
 from asyncio import coroutines, events, protocols
 from asyncio.streams import StreamReader, StreamWriter
@@ -56,106 +50,6 @@ class LimitOverrunError(Exception):
 
     def __reduce__(self):
         return type(self), (self.args[0], self.consumed)
-
-
-async def open_connection(
-    host=None, port=None, *, loop=None, limit=_DEFAULT_LIMIT, **kwds
-):
-    """A wrapper for create_connection() returning a (reader, writer) pair.
-
-    The reader returned is a StreamReader instance; the writer is a
-    StreamWriter instance.
-
-    The arguments are all the usual arguments to create_connection()
-    except protocol_factory; most common are positional host and port,
-    with various optional keyword arguments following.
-
-    Additional optional keyword arguments are loop (to set the event loop
-    instance to use) and limit (to set the buffer limit passed to the
-    StreamReader).
-
-    (If you want to customize the StreamReader and/or
-    StreamReaderProtocol classes, just copy the code -- there's
-    really nothing special here except some convenience.)
-    """
-    if loop is None:
-        loop = events.get_event_loop()
-    reader = StreamReader(limit=limit, loop=loop)
-    protocol = StreamReaderProtocol(reader, loop=loop)
-    transport, _ = await loop.create_connection(lambda: protocol, host, port, **kwds)
-    writer = StreamWriter(transport, protocol, reader, loop)
-    return reader, writer
-
-
-async def start_server(
-    client_connected_cb,
-    host=None,
-    port=None,
-    *,
-    loop=None,
-    limit=_DEFAULT_LIMIT,
-    **kwds
-):
-    """Start a socket server, call back for each client connected.
-
-    The first parameter, `client_connected_cb`, takes two parameters:
-    client_reader, client_writer.  client_reader is a StreamReader
-    object, while client_writer is a StreamWriter object.  This
-    parameter can either be a plain callback function or a coroutine;
-    if it is a coroutine, it will be automatically converted into a
-    Task.
-
-    The rest of the arguments are all the usual arguments to
-    loop.create_server() except protocol_factory; most common are
-    positional host and port, with various optional keyword arguments
-    following.  The return value is the same as loop.create_server().
-
-    Additional optional keyword arguments are loop (to set the event loop
-    instance to use) and limit (to set the buffer limit passed to the
-    StreamReader).
-
-    The return value is the same as loop.create_server(), i.e. a
-    Server object which can be used to stop the service.
-    """
-    if loop is None:
-        loop = events.get_event_loop()
-
-    def factory():
-        reader = StreamReader(limit=limit, loop=loop)
-        protocol = StreamReaderProtocol(reader, client_connected_cb, loop=loop)
-        return protocol
-
-    return await loop.create_server(factory, host, port, **kwds)
-
-
-if hasattr(socket, "AF_UNIX"):
-    # UNIX Domain Sockets are supported on this platform
-
-    async def open_unix_connection(
-        path=None, *, loop=None, limit=_DEFAULT_LIMIT, **kwds
-    ):
-        """Similar to `open_connection` but works with UNIX Domain Sockets."""
-        if loop is None:
-            loop = events.get_event_loop()
-        reader = StreamReader(limit=limit, loop=loop)
-        protocol = StreamReaderProtocol(reader, loop=loop)
-        transport, _ = await loop.create_unix_connection(lambda: protocol, path, **kwds)
-        writer = StreamWriter(transport, protocol, reader, loop)
-        return reader, writer
-
-    async def start_unix_server(
-        client_connected_cb, path=None, *, loop=None, limit=_DEFAULT_LIMIT, **kwds
-    ):
-        """Similar to `start_server` but works with UNIX Domain Sockets."""
-        if loop is None:
-            loop = events.get_event_loop()
-
-        def factory():
-            reader = StreamReader(limit=limit, loop=loop)
-            protocol = StreamReaderProtocol(reader, client_connected_cb, loop=loop)
-            return protocol
-
-        return await loop.create_unix_server(factory, path, **kwds)
 
 
 class FlowControlMixin(protocols.Protocol):
